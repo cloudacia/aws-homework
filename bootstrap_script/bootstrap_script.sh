3783cd7ab397138bd723ca08070f28e41fd49fa5
Eugenio Duarte <eduarte@Eugenios-MacBook-Pro.local>
2022-05-18 00:21:24 -0400

    AWS Fundamentals

--- bootstrap_script/bootstrap_script.sh
+++ bootstrap_script/bootstrap_script.sh
@@ -1,7 +1,1 @@
 #!/bin/bash
-apt-get -y update
-apt-get -y install nginx
-service nginx start
-EC2_HOSTNAME=$(curl -s http://169.254.169.254/latest/meta-data/hostname)
-echo "<h1>Hello AWS world</h1>" > /var/www/html/index.html
-service nginx restart
--- computing.tf
+++ computing.tf
@@ -7,12 +7,12 @@ resource "aws_instance" "web01" {
     user = "user-key"
     host = self.public_ip
   }
-  ami                    = var.aws_amis
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.user_key.key_name
-  vpc_security_group_ids = [aws_security_group.web_and_ssh.id]
-  subnet_id              = aws_subnet.subnet01.id
-  #user_data                   = filebase64("bootstrap_script/bootstrap_script.sh")
+  ami                         = var.aws_amis
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.user_key.key_name
+  vpc_security_group_ids      = [aws_security_group.web_and_ssh.id]
+  subnet_id                   = aws_subnet.subnet01.id
+  user_data                   = filebase64("bootstrap_script/bootstrap_script.sh")
   associate_public_ip_address = true
   availability_zone           = var.availability_zone01
 
